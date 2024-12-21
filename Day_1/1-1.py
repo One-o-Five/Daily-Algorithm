583d7ffec8f0b8f9a35adb2f2031e406ad310fdb
ungraduated <macho2014@naver.com>
2024-12-21 20:13:21 +0900

    Day 1
    1-1.
    3의 배수와 5의 배수의 총합 구하기
    
    1-2.
    피보나치 수열 중 짝수의 합 구하기

---
+++ Day_1/1-1.py
@@ -0,0 +1,30 @@
+# 10 미만의 자연수에서 3과 5의 배수를 구하면 3, 5, 6, 9이다. 
+# 이들의 총합은 23이다.
+# 1,000 미만의 자연수에서 3의 배수와 5의 배수의 총합을 구하라.
+
+
+# 1. 3의 배수와 5의 배수를 전부 리스트 한다.
+# 2. 3과 5의 공배수(=중복되는 숫자)는 1번 제거한다.
+# 3. 리스트를 전부 합친다, sum을 사용하면 됨.
+
+third = [] # 3의 배수의 리스트
+fifth = [] # 5의 배수의 리스트
+
+for i in range(1, 1000):
+    if i % 3 == 0 :
+        third.append(i)  
+
+for i in range(1, 1000):
+    if i % 5 == 0 :
+        fifth.append(i) 
+
+filtering = list(set(third + fifth)) # 리스트 2개를 합쳐서 중복 값을 제거 (list -> data -> list)
+answer = sum(filtering)  # 중복값이 사라진 리스트의 값을 전부 합침침
+
+result = 0
+for n in range(1, 1000):
+    if n % 3 == 0 or n % 5 == 0: 
+        result += n
+print(result)
+
+print(answer)
---
+++ Day_1/1-2.py
@@ -0,0 +1,37 @@
+# 400만 이하의 피보나치 수열 중 짝수의 합을 구하라
+fib = []
+even_fib = []
+sum_even_fib = 0
+
+def func(n):
+    a, b = 1, 1
+    if n == 1 or n == 2:
+        return 1
+    for i in range(1, n):
+        a, b = b, a+b
+    return a
+
+i = 0
+while func(i) <= 4000000:
+    i = i+1
+    fib.append(func(i))
+    if func(i) % 2 == 0:
+        sum_even_fib += func(i)
+        even_fib.append(func(i))
+
+
+# x, y = 0, 0
+# while y <= 4000000:
+#     if y == 0:
+#         x, y = 1, 1
+#         fib = [1, 1]
+#     x, y = y, x+y
+#     fib.append(y)
+#     if y % 2 == 0 :
+#         even_fib.append(y)
+#         sum_even_fib += y
+
+        
+print(fib)
+print(even_fib)
+print(sum_even_fib)
