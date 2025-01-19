0291cf53d41bce89501c4f2bc26d104e87656e25
One-o-Five <macho2014@naver.com>
2025-01-19 19:37:39 +0900

    Day 29 with Python

---
+++ Day21~30/Day_29.py
@@ -0,0 +1,31 @@
+# Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+# 각 자리 숫자를 5제곱해서 더했을 때 자기 자신이 되는 수들의 합을 구하는 문제이다.
+
+# 1. 각 자리의 숫자를 5제곱해서 더했을 때의 최대값을 구해보면 6자리 까지만 찾아보면 됨을 알 수 있다.
+# 2. 그 합의 범위에서 반복문을 돌려준다.
+
+# 각 자리 숫자를 5제곱해서 더했을 때 자기 자신이 되는 수를 찾는 함수
+def digit_fifth_powers():
+    result = []
+
+    # 1부터 9^5 * 6 (6자리 수 최대값)까지 확인
+    upper_limit = 9**5 * 6
+    for num in range(2, upper_limit):
+        digit_sum = 0
+        
+        for digit in str(num):
+            digit_sum += int(digit)**5
+        
+        if num == digit_sum:
+            result.append(num)
+                
+    return result
+
+# 결과 계산
+numbers = digit_fifth_powers()
+result_sum = sum(numbers)
+
+# 결과 출력
+print(numbers)
+print(result_sum)
